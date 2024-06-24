cbedf973160eb9779720f8a027472b64bb99daa7
Satyatma Winarga <53258224+swinarga@users.noreply.github.com>
2024-06-24 10:36:07 +0200

    listing component

--- frontend/src/components/Listing.jsx
+++ frontend/src/components/Listing.jsx
@@ -1,9 +1,74 @@
+import "./Listing.css";
+import burningFlamesHongKongCone from "/hkc_bf.png";
+
 export default function Listing(props) {
 	return (
-		<div className="listing">
-			<div className="listing-want"></div>
-			<div className="listing-for"></div>
-			<div className="listing-desc"></div>
+		<div className="container text-center listing">
+			<div className="row d-flex align-items-stretch">
+				<div className="col-sm p-0">
+					<div className="container listing-want">
+						<div className="row">
+							<div className="listing-header d-flex flex-row p-3 gap-3">
+								<a href="">hohinso</a>
+								<span>wants to trade:</span>
+							</div>
+						</div>
+						<div className="row">
+							<ul className="item-container d-flex flex-row align-items-center">
+								<li
+									className="item"
+									style={{
+										backgroundImage: `url(${burningFlamesHongKongCone})`,
+									}}
+								></li>
+								<li
+									className="item"
+									style={{
+										backgroundImage: `url(${burningFlamesHongKongCone})`,
+									}}
+								></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm p-0">
+					<div className="container listing-for">
+						<div className="row">
+							<div className="listing-header d-flex flex-row p-3 gap-3">
+								<span className="me-auto">for:</span>
+								<span>3 seconds ago</span>
+							</div>
+						</div>
+						<div className="row">
+							<ul className="item-container d-flex flex-row align-items-center">
+								<li
+									className="item"
+									style={{
+										backgroundImage: `url(${burningFlamesHongKongCone})`,
+									}}
+								></li>
+								<li
+									className="item"
+									style={{
+										backgroundImage: `url(${burningFlamesHongKongCone})`,
+									}}
+								></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="row listing-desc p-3">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Suspendisse id convallis sapien. Cras elementum vulputate erat
+				id lobortis. Nunc nisl diam, placerat eget quam sed, cursus
+				consequat ex. Fusce porta vehicula condimentum. Etiam urna
+				lectus, ultricies at arcu a, venenatis mollis tortor. Lorem
+				ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+				facilisis sem eget turpis pharetra consequat id maximus orci.
+				Etiam convallis risus eu posuere consequat. Donec elit justo,
+				consequat id orci nec, fermentum consequat justo.
+			</div>
 		</div>
 	);
 }
