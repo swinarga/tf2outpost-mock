e5d2676ebb2bb302e8ad10d2ce5124c459f61ca4
Satyatma Winarga <53258224+swinarga@users.noreply.github.com>
2024-06-27 01:13:52 +0200

    frontend - inventory item description popover on hover

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useRef } from "react";
+import { renderToString } from "react-dom/server";
 import "./Item.css";
+import { Popover } from "bootstrap";
+
+function createItemDescription(item) {
+	return (
+		<div>
+			<span>{item.name}</span>
+		</div>
+	);
+}
 
 export default function Item(props) {
+	const popoverRef = useRef();
 	// const item = props.item;
 	// const itemName = props.itemName;
 	// const itemId = props.item.original_id;
@@ -14,12 +26,27 @@ export default function Item(props) {
 	// const itemKillstreaker = props.item.killstreaker;
 	// const itemPaint = props.item.paint;
 
+	useEffect(() => {
+		const popover = new Popover(popoverRef.current, {
+			placement: "bottom",
+			trigger: "hover",
+			html: true,
+			content: renderToString(createItemDescription(props.item)),
+		});
+
+		return () => {
+			popover.dispose();
+		};
+	}, [props.item]);
+
 	return (
-		<div
+		<span
 			className="item"
-			style={{ backgroundImage: `url(${props.item.image_url})` }}
-		>
-			{/* props.item.name */}
-		</div>
+			style={{
+				backgroundImage: `url(${props.item.image_url})`,
+				border: `1px solid ${props.item.quality.color}`,
+			}}
+			ref={popoverRef}
+		></span>
 	);
 }
