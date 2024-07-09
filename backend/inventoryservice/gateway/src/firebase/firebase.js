52c15138f9ea492b0003cc37a04c208ec377ef52
Satyatma Winarga <53258224+swinarga@users.noreply.github.com>
2024-07-09 23:00:49 +0200

    inventoryservice - serviceAccountKey.json moved for deployment

--- backend/inventoryservice/gateway/src/firebase/firebase.js
+++ backend/inventoryservice/gateway/src/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "../../serviceAccountKey.json" assert { type: "json" };
+import serviceAccount from "../../../serviceAccountKey.json" assert { type: "json" };
 
 // Initialize Firebase
 const app = admin.initializeApp({
